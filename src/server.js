3279fafebe80fd695bcbf59a365d96e99c83e0c5
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-01-06 10:13:10 +0900

    add morgan

--- src/server.js
+++ src/server.js
@@ -4,17 +4,39 @@ const PORT = 4000;
 const app = express();
 
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+const urlLogger = (req, res, next) => {
+  console.log(`Path: ${req.path}`);
   next();
-};
+}
+
+const timeLogger = (req, res, next) => {
+  const date = new Date();
+  console.log(`Time: ${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`);
+  next();
+}
+
+const securityLogger = (req, res, next) => {
+  if (req.protocol === "https") {
+    console.log("secure")
+  } else {
+    console.log("insecure")
+  }
+  next();
+}
+
+const protectorLogger = (req, res, next) => {
+  if (req.path === "/protected") {
+    res.send("you can't go there")
+  }
+  next();
+}
 
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-
-app.get("/", logger, handleHome);
+app.use(urlLogger, timeLogger, securityLogger, protectorLogger);
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
