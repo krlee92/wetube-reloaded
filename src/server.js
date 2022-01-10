19d59d03c8582f3c08327bd96168bd30dd052e76
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-01-10 13:37:10 +0900

    pug, rendering

--- src/server.js
+++ src/server.js
@@ -1,42 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
+app.set("view engine", "pug")
+app.set("views", process.cwd() + "/src/views")
+app.use(logger);
 
-const urlLogger = (req, res, next) => {
-  console.log(`Path: ${req.path}`);
-  next();
-}
-
-const timeLogger = (req, res, next) => {
-  const date = new Date();
-  console.log(`Time: ${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`);
-  next();
-}
-
-const securityLogger = (req, res, next) => {
-  if (req.protocol === "https") {
-    console.log("secure")
-  } else {
-    console.log("insecure")
-  }
-  next();
-}
-
-const protectorLogger = (req, res, next) => {
-  if (req.path === "/protected") {
-    res.send("you can't go there")
-  }
-  next();
-}
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-app.use(urlLogger, timeLogger, securityLogger, protectorLogger);
-app.get("/", handleHome);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
