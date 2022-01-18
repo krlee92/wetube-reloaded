af310495889797f69bb81a9832556255de7777fa
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-01-18 15:48:21 +0900

    watch Video and 정규식 regular expression

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,10 +9,11 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 /*videoRouter.get("/:id(\\d+)/edit", getEdit);
 videoRouter.post("/:id(\\d+)/edit", postEdit); 한줄로 가능*/
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
