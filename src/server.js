b6dde8f97527ad5370da67cc7159ef58fedc20ce
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-01-21 15:25:58 +0900

    add logIn with locals

--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-
-
+import { localsMiddleware } from "./middlewares";
 const app = express();
 const logger = morgan("dev");
-
-app.set("view engine", "pug")
-app.set("views", process.cwd() + "/src/views")
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
- 
 export default app;
