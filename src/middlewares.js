1a1278c79d9958f5fbfb86cfdb59c55fe6347a47
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-01-24 22:06:38 +0900

    add MongoStore
    to save session-data in MongoDB, install MongoStore and set Settings

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,8 @@
 export const localsMiddleware = (req, res, next) =>{
     
     res.locals.loggedIn = Boolean(req.session.loggedIn);
-    
     res.locals.siteName = "Wetube";
-    console.log(res.locals);
+    res.locals.loggedInUser = req.session.user;
+    
     next();
 }
