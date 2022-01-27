0d54a26680ae6e1d8b40dce9ba94fb5b4671b8b6
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-01-27 16:58:23 +0900

    relationship between Video and User
    to add Schema for each models => type: mongoose.Schema.Types.ObjectId,
    mongoose can help us to find more information with each ids.
    what we doing is add chema->ObjectId and use populate()

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 import Video from "../models/Video";
-
+import User from "../models/User";
 /*
 console.log("start")
 Video.find({}, (error, videos) => {
@@ -18,7 +18,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id);
+  
+  const video = await Video.findById(id).populate("owner");
   if (!video) {
     return res.status(400).render("404", { pageTitle: "Video not found." });
   }
@@ -53,15 +54,22 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
-    await Video.create({
+    const newVideo = await Video.create({
       title,
       description,
       fileUrl,
+      owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
+    const user = await User.findById(_id);
+    user.videos.push(newVideo._id);
+    user.save();
     return res.redirect("/");
   } catch (error) {
     console.log(error);
