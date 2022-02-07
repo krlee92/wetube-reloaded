f689758400efcec7e94533094d51a0e3e5e21401
kangsan lee <65530351+krlee92@users.noreply.github.com>
2022-02-07 15:39:17 +0900

    using Blob to convert webm video to mp4

--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+  });
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -26,6 +31,7 @@ app.use(
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
+app.use("/convert", express.static("node_modules/@ffmpeg/core/dist"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
